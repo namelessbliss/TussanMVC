3507e6c6212d8b927fe92989e6ffb9e07b0d0e1b
namelessbliss <anonimo2911.2012@gmail.com>
2019-11-24 18:22:50 -0500

    Subida Inicial, MasterPage establecido

---
+++ TUSSAN-TUNQUI/Controllers/AdmController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TUSSAN_TUNQUI.Controllers
+{
+    public class AdmController : Controller
+    {
+        // GET: Adm
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TUSSAN-TUNQUI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TUSSAN_TUNQUI.Models;
+
+
+namespace TUSSAN_TUNQUI.Controllers {
+    public class HomeController : Controller {
+
+
+        [HttpGet]
+        public ActionResult Index() {
+            return View();
+        }
+
+        /*
+        public ActionResult IntranetAdmin()
+        {
+            string nombre = Request.Form["txtnom"].ToString();
+            string contraseña = Request.Form["txtcontra"].ToString();
+
+            usuario usu = new usuario();
+            usu.login(nombre, contraseña);
+            string sql = string.Format("select * from Empleado where usuario = '"+nombre+"' and contraseña = '"+contraseña+"'");
+            DB db = new DB();
+            db.transaccion(sql);            
+            return View();
+        }              
+        */
+
+
+    }
+}
